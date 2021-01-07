6d91e208659c297f4a9af5ed7df73377d2de2311
“ivan_p” <vfvf6462@gmail.com>
2021-01-07 23:24:46 +0300

    Добавление файлов в Github [#3]

---
+++ src/ru/ivan/StudentInfo.java
@@ -0,0 +1,9 @@
+package ru.ivan;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+
+        System.out.println("ivan_p");
+        System.out.println("01.01.2021");
+    }
+}
