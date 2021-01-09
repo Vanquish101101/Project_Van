f1f033430a332da66834bf9cb0ce93b1da86784a
“ivan_p” <vfvf6462@gmail.com>
2021-01-09 20:05:58 +0300

    Добавление файлов в Github [#9] - арифметические действия

---
+++ src/ru/calculate/Calculator.java
@@ -0,0 +1,30 @@
+package ru.calculate;
+
+public class Calculator {
+    public static void main(String[] args) {
+        int one = 1;
+        int two = 2;
+        int result = one + two;
+
+        Sysyem.out.println(result);
+
+        System.out.println("Д.З. операции с переменными");
+
+        int six = 6;
+        int four = 4;
+        int five = 5;
+
+        int res_1;
+        int res_2;
+        int res_3;
+
+        res_1 = six / 2;
+        res_2 = five - 2;
+        res_3 = four * 2;
+
+        System.out.println(res_1);
+        System.out.println(res_2);
+        System.out.println(res_3);
+
+    }
+}
