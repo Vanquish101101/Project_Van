e74e3c84537648d18a54737ce68af1fcc5debee7
“ivan_p” <vfvf6462@gmail.com>
2021-01-10 16:35:50 +0300

    [#13] - типы данных

--- src/ru/calculate/Methods.java
+++ src/ru/calculate/Methods.java
@@ -1,10 +1,8 @@
 package ru.calculate;
-
 public class Methods {
     public static void plus(int first, int second) {
         int result;
         result = first + second;
         System.out.println(result);
-
     }
 }
--- src/ru/calculate/Multiple.java
+++ src/ru/calculate/Multiple.java
@@ -1,7 +1,5 @@
 package ru.calculate;
-
 public class Multiple {
-
     public static void main(String[] args) {
         System.out.println("2 * 1 = 2");
         System.out.println("2 * 2 = 4");
