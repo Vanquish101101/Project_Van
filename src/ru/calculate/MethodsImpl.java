50e34f2fa137f80328d709e10010d769a137599c
“ivan_p” <vfvf6462@gmail.com>
2021-01-20 00:31:39 +0300

    [17] - Понятие метода, и непонимание чего делаю ?! )))

--- src/ru/calculate/MethodsImpl.java
+++ src/ru/calculate/MethodsImpl.java
@@ -1,8 +1,9 @@
 package ru.calculate;
-public class Methods {
+public class MethodsImpl {
     public static void plus(int first, int second) {
         int result;
         result = first + second;
         System.out.println(result);
     }
+    MethodsImpl.plus(1,2);
 }
