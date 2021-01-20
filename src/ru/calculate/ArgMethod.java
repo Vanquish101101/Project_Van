d2bbdd6a39a3cb7a28dc91f5a0b4a40f1a539acf
“ivan_p” <vfvf6462@gmail.com>
2021-01-21 00:18:33 +0300

    [19] - Добавлен метод CalcMethod. изм назв

--- src/ru/calculate/ArgMethod.java
+++ src/ru/calculate/ArgMethod.java
@@ -2,6 +2,6 @@ package ru.calculate;
 
 public class ArgMethod {
     public static void hello(String name) {
-        System.out.println("Hello", + name);
+        System.out.println("Hello" + name);
     }
 }
--- src/ru/calculate/CalcMethod.java
+++ src/ru/calculate/CalcMethod.java
@@ -1,9 +1,10 @@
 package ru.calculate;
 public class CalcMethod {
     public static void plus(int first, int second) {
-        int result;
-        result = first + second;
+        int result = first + second;
         System.out.println(result);
     }
-    CalcMethod.plus(1,2);
+    public static void main(String[] args) {
+        CalcMethod.plus(1, 2);
+    }
 }
