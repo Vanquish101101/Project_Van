9c2ec596839366c553108d5ebc4c31bad0b3c2f3
“ivan_p” <vfvf6462@gmail.com>
2021-01-21 00:32:15 +0300

    [20] - Разбор аргументов и типов

--- src/ru/calculate/ArgMethod.java
+++ src/ru/calculate/ArgMethod.java
@@ -1,7 +1,11 @@
 package ru.calculate;
-
 public class ArgMethod {
     public static void hello(String name) {
-        System.out.println("Hello" + name);
+        System.out.println("Hello " + name);
+    }
+
+    public static void main(String[] args) {
+        String name = "Terminator";
+        ArgMethod.hello(name);
     }
 }
