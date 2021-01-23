f4e947a94df0808098d3d613c2ff36347f5b17fd
“ivan_p” <vfvf6462@gmail.com>
2021-01-23 22:21:12 +0300

    [22] - Создание конвертера валют

---
+++ src/ru/calculate/Сonverter.java
@@ -0,0 +1,18 @@
+package ru.calculate;
+
+public class Сonverter {
+    public static int rubleToEuro(int value) {
+        int rsl = value / 70; /* формула перевода рублей в евро */
+        return rsl;
+    }
+    public static int rubleToDollar (int value) {
+        int rsl = value / 60; /* формула перевода рублей в доллары */
+        return rsl;
+    }
+    public static void main (String[] args) {
+        int euro = Converter.rubleToEuro(140);
+        int dollar = Converter.rubleToEuro(70);
+        System.out.println("140 rubles are " + euro + " euro(s)");
+        System.out.println("70 rubles are " + dollar + " dollar(s)");
+    }
+}
