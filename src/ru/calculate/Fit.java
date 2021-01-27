f6cfc0e9a1a3121027e9fac0e61d7270207d4460
“ivan_p” <vfvf6462@gmail.com>
2021-01-27 23:13:32 +0300

    [23] - Идеальный вес

--- src/ru/calculate/Fit.java
+++ src/ru/calculate/Fit.java
@@ -2,18 +2,17 @@ package ru.calculate;
 
 public class Fit {
     public static double manWeight (short height) {
-        double rslt = -100 * 1.15;
-        return rslt;
+        return height - 100 * 1.15;
+
     }
     public static double womanWeight (short height) {
-        double rslt = -110 * 1.15;
-        return rslt;
+        return height - 110 * 1.15;
     }
     public static void main (String[] args) {
-        short height_man = 187;
-        short height_woman = 163;
-        double man = Fit.manWeight(height_man);
-        double woman = Fit.womanWeight(height_woman);
+        short manHeight = 163;
+        short womanHeight = 187;
+        double man = Fit.manWeight(manHeight);
+        double woman = Fit.womanWeight(womanHeight);
         System.out.println("Man 187 is " + man);
         System.out.println("Woman 163 is " + woman);
     }
