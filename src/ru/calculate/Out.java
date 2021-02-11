5dee722aec0f69767be5af9cdb4a7d1033c655d8
unknown_user <sergantforce@ya.ru>
2021-02-12 00:38:53 +0300

    [] - String. Работа со строковыми данными

---
+++ src/ru/calculate/Out.java
@@ -0,0 +1,23 @@
+package ru.calculate;
+
+public class Out {
+    public static void main(String[] args) {
+        String lyrics = "Мама мыла раму";
+        System.out.println(lyrics);
+
+        String name = "Ivan";
+        String surname = "Terminatorov";
+        String separate = " ";
+        String id = name + separate + surname;
+        System.out.println(id);
+
+        int first = 1;
+        int second = 2;
+        int onePlusTwo = first + second;
+        System.out.println("1 + 2 = " + onePlusTwo);
+
+    }
+
+}
+
+
