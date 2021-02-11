53bc72cd84b26efc6d546c899e7e7186af9dc9ab
unknown_user <sergantforce@ya.ru>
2021-02-12 00:55:14 +0300

    [] - String. Работа со строковыми данными

---
+++ src/ru/condition/Greeting.java
@@ -0,0 +1,12 @@
+package ru.condition;
+
+public class Greeting {
+    public static void main(String[] args) {
+        String idea = "I Like Java";
+        System.out.println(idea + " " + "But I am a newbie.");
+
+        int year = 2021;
+        String probel = " ";
+        System.out.println(idea + probel + year);
+    }
+}
