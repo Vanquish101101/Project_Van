31c8e14de9b2d9bd0b6498dae44299e2ccee00bc
Project_Van <sergantforce@ya.ru>
2021-04-29 21:51:51 +0300

    [] - Модульные тетсы

--- src/ru/test/calculate/ConverterTest.java
+++ src/ru/test/calculate/ConverterTest.java
@@ -1,24 +1,5 @@
 package calculate;
 
-//import org.hamcrest.Matcher;
-//
-//import static org.junit.Assert.*;
-//import static org.hamcrest.core.Is.is;
-//import org.junit.Test;
-//
-//public class ConverterTest {
-//
-//    @Test
-//    public void rubleToDollar() {
-//        assertThat(Converter.rubleToDollar(120), is(2));
-//    }
-//
-//    @Test
-//    public void rubleToEuro() {
-//        assertThat(Converter.rubleToDollar(140), is(2));
-//    }
-//}
-
 import org.junit.Assert;
 import org.junit.Test;
 
---
+++ src/ru/test/calculate/ConverterTestVer2.java
@@ -0,0 +1,20 @@
+package calculate;
+
+import org.hamcrest.Matcher;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.core.Is.is;
+import org.junit.Test;
+
+public class ConverterTestVer2 {
+
+    @Test
+    public void rubleToDollar() {
+        assertThat(Converter.rubleToDollar(120), is(2));
+    }
+
+    @Test
+    public void rubleToEuro() {
+        assertThat(Converter.rubleToDollar(140), is(2));
+    }
+}
