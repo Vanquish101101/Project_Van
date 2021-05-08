b3068a48807aaa1e3cd3772b01c7fec37d46b039
Project_Van <sergantforce@ya.ru>
2021-05-08 21:22:46 +0300

    [] - Оператор if с блоком else

--- src/ru/condition/AlertDivByZero.java
+++ src/ru/condition/AlertDivByZero.java
@@ -4,20 +4,18 @@ public class AlertDivByZero {
     public static void main(String[] args) {
         possibleDiv(4);
         possibleDiv(0);
-        possibleNegativeNumbDiv(-1);
+        possibleNegativeDiv(-1);
 
     }
 
     public static void possibleDiv(int number) {
-        boolean result = number == 0;
-        if (result) {
+        if (number == 0) {
             System.out.println("Could not div by 0.");
         }
     }
 
-    public static void possibleNegativeNumbDiv(int number) {
-        boolean result = number == -1;
-        if (result) {
+    public static void possibleNegativeDiv(int number) {
+        if (number == -1) {
             System.out.println("This is negative numbers");
         }
     }
