6cfd3d31756521be0e219238acf93f30ae31c193
Project_Van <sergantforce@ya.ru>
2021-05-10 01:26:42 +0300

    [] - Оператор логическое НЕ (!)

---
+++ src/ru/condition/LogicNot.java
@@ -0,0 +1,39 @@
+package condition;
+
+public class LogicNot {
+
+    public static boolean isEven(int num) {
+        return num % 2 == 0;
+    }
+
+    public static boolean isPositive(int num) {
+        return num > 0;
+    }
+
+    public static boolean notEven(int num) {
+        return !isEven(num);
+    }
+
+    public static boolean notPositive(int num) {
+        return !isPositive(num);
+    }
+
+    public static boolean notEvenAndPositive(int num) {
+        return !isEven(num) && isPositive(num);
+    }
+
+    public static boolean evenOrNotPositive(int num) {
+        return isEven(num) || !isPositive(num);
+    }
+
+    public static void main (String[] args) {
+        isEven(3);
+        isPositive(1);
+        notEven(7);
+        notPositive(-3);
+        notEvenAndPositive(5);
+        evenOrNotPositive(-5);
+    }
+}
+
+
