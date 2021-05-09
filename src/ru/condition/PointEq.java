f923ba7b3c2ebb9aed0a951802e420445f8bf00d
Project_Van <sergantforce@ya.ru>
2021-05-10 02:13:39 +0300

    [] - Оператор if  с применением return

--- src/ru/condition/PointEq.java
+++ src/ru/condition/PointEq.java
@@ -1,15 +1,11 @@
 package condition;
 
-public class PointEqNotSimple {
+public class PointEq {
 
     public static boolean eq(int x1, int y1, int x2, int y2) {
         boolean eqX = x1 == x2;
         boolean eqY = y1 == y2;
-        if (eqX && eqY) {
-            return true;
-        } else {
-            return false;
-        }
+        return  eqX && eqY ? true : false;
     }
 
     public static void main(String[] args) {
@@ -19,4 +15,3 @@ public class PointEqNotSimple {
 }
 
 
-//[] - Оператор if  с применением return. До упрощения кода
