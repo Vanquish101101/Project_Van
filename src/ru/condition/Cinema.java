87aa360753673cda7fe2560186b73d9846a34618
Project_Van <sergantforce@ya.ru>
2021-05-13 00:13:10 +0300

    [] - Оператор сравнения в String

--- src/ru/condition/Cinema.java
+++ src/ru/condition/Cinema.java
@@ -2,7 +2,6 @@ package condition;
 
 public class Cinema {
     public static void access(int age) {
-//        System.out.println("The age of the customer is : " + age);
         if (age >= 21) {
             System.out.println("Welcome to the cinema.");
         } else {
