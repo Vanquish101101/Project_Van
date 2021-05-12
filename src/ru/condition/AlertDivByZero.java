c7464f3f71a876cb530e8dc2b2c1b9a1d31aafe2
Project_Van <sergantforce@ya.ru>
2021-05-12 23:59:01 +0300

    [] - Множественное логическое выражение "И"

--- src/ru/condition/AlertDivByZero.java
+++ src/ru/condition/AlertDivByZero.java
@@ -4,19 +4,11 @@ public class AlertDivByZero {
     public static void main(String[] args) {
         possibleDiv(4);
         possibleDiv(0);
-        possibleNegativeDiv(-1);
-
     }
 
     public static void possibleDiv(int number) {
         if (number == 0) {
             System.out.println("Could not div by 0.");
-        }
-    }
-
-    public static void possibleNegativeDiv(int number) {
-        if (number == -1) {
-            System.out.println("This is negative numbers");
         }
     }
 }
---
+++ src/ru/yourself/BoolLogic.java
@@ -0,0 +1,24 @@
+//package yourself;
+//
+//public class BoolLogic {
+//    public static void max(boolean left, boolean right) {
+//        boolean result = left && right ? System.out.println("true") : System.out.println("false");
+//        return result;
+//    }
+//
+////        if ( left && right) {
+////            System.out.println("true");
+////        } else {
+////            System.out.println("false");
+////        }
+//    }
+//
+//    public static void main(String[] args) {
+//        max(true, true);
+//        max(true, false);
+//        max(false, true);
+//        max(false, false);
+//    }
+//}
+
+
